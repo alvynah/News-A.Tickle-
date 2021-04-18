1b98aa1bafbf09fca5f71e42c79c09dc6cda8e95
alvynah <alvynahw@gmail.com>
2021-04-18 11:31:47 +0300

    Making an API call for sources

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,34 @@
 from app import app
+import urllib.request,json
+from .models import newsources
+Sources = newsources.Sources
+
+
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = app.config['NEWS_API_KEY'
+
+#Getting Sources base url
+base_url=app.config["NEWS_API_BASE_URL"]
+
+def get_newsSources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_sources_data= url.read()
+        get_sources_response= json.loads(get_sources_data)
+
+        sources_results= None
+
+        if get_sources_response['sources']:
+            sources_results_list=  get_sources_response['sources']
+            sources_results= process_sources_results(sources_results_list)
+
+
+    return sources_results
+
+
+
