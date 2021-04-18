a8b69baa84e6e391cf198fc0b1604d5da43d40f0
alvynah <alvynahw@gmail.com>
2021-04-18 09:58:22 +0300

    Add a page for news sources and news articles

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from app import app
+
+# Views
+
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
+
+@app.route('/newsources/<int:news_id>')
+def newsources(news_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('newsources.html', id=news_id)
