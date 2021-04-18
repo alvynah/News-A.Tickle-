5a4b272e2b5407b4f82d2665ae9b55f13fcde7ce
alvynah <alvynahw@gmail.com>
2021-04-18 11:05:20 +0300

    Add a class for news sources and create a test to test the class

---
+++ app/test_sources.py
@@ -0,0 +1,23 @@
+import unittest
+from models import newsources
+Sources = newsources.Sources
+
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Sources class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources("usa-today", "USA Today", "Get the latest national, international, and political news at USATODAY.com.",
+                                  "http://www.usatoday.com/news", "general", "en", "us")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Sources))
+
+
+if __name__ == '__main__':
+    unittest.main()
